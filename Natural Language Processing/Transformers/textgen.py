1914fae9c68a3647b1dd68a577e9ecdfc50efefd
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-10-19 01:43:07 +0200

    Testing Llama model.

--- Natural Language Processing/Transformers/textgen.py
+++ Natural Language Processing/Transformers/textgen.py
@@ -1,11 +1,21 @@
+# from transformers import pipeline
+
+# generator = pipeline(task="text-generation", model="gpt2")
+
+# prompt = "In the future I want to become"
+
+# output = generator(prompt, max_length=50, num_return_sequences=1)
+
+# print("\n\n\n\n\n")
+
+# print(output[0]['generated_text'])
+
+
+# Use a pipeline as a high-level helper
 from transformers import pipeline
 
-generator = pipeline(task="text-generation", model="gpt2")
-
-prompt = "In the future I want to become"
-
-output = generator(prompt, max_length=50, num_return_sequences=1)
-
-print("\n\n\n\n\n")
-
-print(output[0]['generated_text'])
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="nvidia/Llama-3.1-Nemotron-70B-Instruct-HF")
+pipe(messages)
